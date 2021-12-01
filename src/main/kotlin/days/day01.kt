29c5bb223c05ac96f891a42566fdbe1fa887ca94
Ivo_Broekhof <i.broekhof@student.utwente.nl>
2021-12-01 10:03:02 +0100

    Day 1!

--- src/main/kotlin/days/day01.kt
+++ src/main/kotlin/days/day01.kt
@@ -1,5 +1,24 @@
 package days
 
+import java.io.File
+
 fun main() {
-    
+    // Import and parsing
+    val file = File("src/main/kotlin/inputs/day01.txt")
+    val nums = file.readLines().map(String::toInt)
+
+    // Part 1
+    println(
+        nums.countIncrease()
+    )
+
+    // Part 2
+    println(nums
+            .windowed(3)
+            .map { it.sum() }.countIncrease()
+    )
 }
+
+private fun List<Int>.countIncrease() = this
+    .windowed(2)
+    .count { it[0] < it[1] }
