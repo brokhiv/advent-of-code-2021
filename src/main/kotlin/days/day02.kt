464ba3cf585981be05dd05b1759551d28aa7982b
Ivo_Broekhof <i.broekhof@student.utwente.nl>
2021-12-02 09:26:59 +0100

    Day 2 solved!

--- src/main/kotlin/days/day02.kt
+++ src/main/kotlin/days/day02.kt
@@ -4,4 +4,45 @@ import java.io.File
 
 fun main() {
     val file = File("src/main/kotlin/inputs/day02.txt")
+
+    val directions = file.readLines().map { it.split(" ")}
+
+    partOne(directions)
+    partTwo(directions)
+}
+
+fun partTwo(directions: List<List<String>>) {
+    var depth = 0
+    var horizontal = 0
+    var aim = 0
+
+    for (dir in directions) {
+        when (dir[0]) {
+            "up" -> {
+                aim -= dir[1].toInt()
+            }
+            "down" -> {
+                aim += dir[1].toInt()
+            }
+            "forward" -> {
+                horizontal += dir[1].toInt()
+                depth += aim * dir[1].toInt()
+            }
+        }
+    }
+    println("Part 2: ${horizontal * depth}")
+}
+
+fun partOne(directions: List<List<String>>) {
+    var depth = 0
+    var horizontal = 0
+
+    for (dir in directions) {
+        when (dir[0]) {
+            "up" -> depth -= dir[1].toInt()
+            "down" -> depth += dir[1].toInt()
+            "forward" -> horizontal += dir[1].toInt()
+        }
+    }
+    println("Part 1: ${horizontal * depth}")
 }
