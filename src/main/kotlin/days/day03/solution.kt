20ac68c5765de2cf100f41fa5966bc0933c8c5a7
Ivo_Broekhof <i.broekhof@student.utwente.nl>
2021-12-05 16:33:25 +0100

    Improved day 3 to go var-free

--- src/main/kotlin/days/day03/solution.kt
+++ src/main/kotlin/days/day03/solution.kt
@@ -9,17 +9,22 @@ fun main() {
         .listFiles { _, name -> name.endsWith(".txt") }
         .forEach {
             println("Input: ${it.name}")
-            val text = it.readLines().map { it.map { it.digitToInt() } }
+            val text = it.readLines()
             partOne(text)
             partTwo(text)
+            println()
+            partOneBetter(text)
+            partTwoBetter(text)
+            println()
         }
 }
 
-fun partOne(diagnostics: List<List<Int>>) {
+fun partOne(diagnostics: List<String>) {
     var gamma = 0
     var epsilon = 0
 
     diagnostics
+        .map { it.map { it.digitToInt() } }
         .reduce { acc, list ->
             acc.zip(list).map { (a, l) -> a + l }
         }
@@ -33,9 +38,9 @@ fun partOne(diagnostics: List<List<Int>>) {
     println("Part 1: ${gamma * epsilon}")
 }
 
-fun partTwo(diagnostics: List<List<Int>>) {
-    var oxygenNumbers: List<List<Int>> = diagnostics
-    var co2Numbers: List<List<Int>> = diagnostics
+fun partTwo(diagnostics: List<String>) {
+    var oxygenNumbers: List<List<Int>> = diagnostics.map { it.map { it.digitToInt() } }
+    var co2Numbers: List<List<Int>> = diagnostics.map { it.map { it.digitToInt() } }
     for (i in diagnostics[0].indices) {
         if (oxygenNumbers.size > 1) {
             oxygenNumbers = oxygenNumbers.filter {
@@ -47,7 +52,7 @@ fun partTwo(diagnostics: List<List<Int>>) {
                 1 - it[i] == co2Numbers.sumOf { it[i] } * 2 / co2Numbers.size
             }
         }
-        if ((oxygenNumbers.size == 1 && co2Numbers.size == 1) || i + 1 == diagnostics[0].size) {
+        if ((oxygenNumbers.size == 1 && co2Numbers.size == 1) || i + 1 == diagnostics[0].length) {
             break
         }
     }
@@ -61,4 +66,61 @@ fun partTwo(diagnostics: List<List<Int>>) {
     }
 
     println("Part 2: ${oxygenRating * co2Rating}")
+}
+
+fun partOneBetter(diagnostics: List<String>) {
+    val (gamma, epsilon) = diagnostics
+        .fold(List(diagnostics[0].length) { 0 }) { acc, s ->
+            acc.zip(s.toList()).map { (a, c) -> a + if (c == '1') 1 else 0 }
+        }
+        .map { Pair((2 * it / diagnostics.size).toString(), (1 - 2 * it / diagnostics.size).toString()) }
+        .reduce { acc, pair -> Pair(acc.first + pair.first, acc.second + pair.second) }
+
+    println("Part 1 better: ${gamma.toInt(2) * epsilon.toInt(2)}")
+}
+
+private fun analyze(diagnostics: List<String>): Pair<String, String> {
+     tailrec fun go(
+        mostCommon: List<String>,
+        leastCommon: List<String> = mostCommon,
+        i: Int = 0
+    ): Triple<List<String>, List<String>, Int> {
+        if (i >= mostCommon[0].length) return Triple(mostCommon, leastCommon, mostCommon[0].length)
+        return if (mostCommon.size == 1) {
+            if (leastCommon.size == 1) Triple(mostCommon, leastCommon, mostCommon[0].length)
+            else { // Process leastCommon
+                val lcb = leastCommon.count { it[i] == '1' } * 2 <= leastCommon.size
+                go(mostCommon, leastCommon.filter { it[i] == if (lcb) '1' else '0' }, i + 1)
+            }
+        } else { // Process mostCommon
+            val mcb = mostCommon.count { it[i] == '1' } * 2 >= mostCommon.size
+            if (leastCommon.size == 1) {
+                go(mostCommon.filter { it[i] == if (mcb) '1' else '0' }, leastCommon, i + 1)
+            } else {
+                val lcb = leastCommon.count { it[i] == '1' } * 2 < leastCommon.size
+                go (
+                    mostCommon.filter { it[i] == if (mcb) '1' else '0' },
+                    leastCommon.filter { it[i] == if (lcb) '1' else '0' },
+                    i + 1
+                )
+            }
+        }
+    }
+
+    val (most, least, _) = go(diagnostics)
+    return Pair(most[0], least[0])
+}
+
+fun partTwoBetter(diagnostics: List<String>) {
+    val (mostCommonBits, leastCommonBits) = analyze(diagnostics)
+
+    val (oxygen, co2) = Pair(
+        diagnostics.reduce { acc, el ->
+            if (mostCommonBits.commonPrefixWith(acc).length > mostCommonBits.commonPrefixWith(el).length) acc else el
+        }, diagnostics.reduce { acc, el ->
+            if (leastCommonBits.commonPrefixWith(acc).length > leastCommonBits.commonPrefixWith(el).length) acc else el
+        }
+    )
+
+    println("Part 2 better: ${oxygen.toInt(2) * co2.toInt(2)}")
 }
