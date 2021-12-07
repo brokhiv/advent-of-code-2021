4d31703b10d056d264f63df39fcc909e67c79e54
Ivo_Broekhof <i.broekhof@student.utwente.nl>
2021-12-07 10:47:22 +0100

    Solved day 7!

--- src/main/kotlin/days/day07/solution.kt
+++ src/main/kotlin/days/day07/solution.kt
@@ -1,6 +1,8 @@
 package days.day07
 
+import util.countEq
 import java.io.File
+import kotlin.math.abs
 
 fun main() {
     val today = "src/main/kotlin/days/day07"
@@ -9,16 +11,42 @@ fun main() {
         .listFiles { _, name -> name.endsWith(".txt") }
         .forEach {
             println("Input: ${it.name}")
-            val text = it.readText() // TODO processing
+            val text = it.readText().split(",")
             partOne(text)
             partTwo(text)
         }
 }
 
-fun partOne(input: Any) {
-    println("Part 1: Not implemented yet")
+fun partOne(input: List<String>) {
+    val crabs = input.map(String::toInt)
+
+    val positions = Array(crabs.maxOrNull()!! + 1) { 0 }
+    for (i in crabs.minOrNull()!!..crabs.maxOrNull()!!) {
+        positions[i] = crabs.countEq(i)
+    }
+
+    val costs = positions.mapIndexed { i, _ ->
+        positions.foldIndexed(0) { j, acc, count ->
+            acc + (abs(j - i) * count)
+        }
+    }
+
+    println("Part 1: " + costs.minOrNull())
 }
 
-fun partTwo(input: Any) {
-    println("Part 2: Not implemented yet")
+fun partTwo(input: List<String>) {
+    val crabs = input.map(String::toInt)
+
+    val positions = Array(crabs.maxOrNull()!! + 1) { 0 }
+    for (i in crabs.minOrNull()!!..crabs.maxOrNull()!!) {
+        positions[i] = crabs.countEq(i)
+    }
+
+    val costs = positions.mapIndexed { i, _ ->
+        positions.foldIndexed(0) { j, acc, count ->
+            acc + (abs(j - i).let { it * (it + 1) / 2 } * count)
+        }
+    }
+
+    println("Part 2: " + costs.minOrNull())
 }
--- src/main/kotlin/util/UsefulStuff.kt
+++ src/main/kotlin/util/UsefulStuff.kt
@@ -3,6 +3,8 @@ package util
 
 inline fun <T> Iterable<T>.countEq(element: T): Int = this.count { it == element }
 
+inline fun <T, U> Iterable<T>.nMap(fs: List<(T) -> U>): List<U> = this.zip(fs) { x, f -> f(x) }
+
 fun <A,B,C,D> Pair<A,B>.bimap(f: (A) -> C, g: (B) -> D) = Pair(f(first), g(second))
 
 fun <A,B,C,D,E,F> Triple<A,B,C>.trimap(f: (A) -> D, g: (B) -> E, h: (C) -> F) = Triple(f(first), g(second), h(third))
