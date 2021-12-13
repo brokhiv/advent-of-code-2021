2733a76e3c642bae3571847464eaca8b4e164a36
Ivo_Broekhof <i.broekhof@student.utwente.nl>
2021-12-13 14:19:38 +0100

    Made a set-based solution for day 13

--- src/main/kotlin/days/day13/solution.kt
+++ src/main/kotlin/days/day13/solution.kt
@@ -13,6 +13,9 @@ fun main() {
             val text = it.readText() // TODO processing
             partOne(text)
             partTwo(text)
+            println("\n")
+            partOneSet(text)
+            partTwoSet(text)
         }
 }
 
@@ -86,4 +89,73 @@ fun partTwo(input: String) {
 
     val final = folds.fold(sheet) { s, f -> f.apply(s)}
     println("Part 2: \n$final")
+}
+
+data class SetSheet(val dots: Set<Pair<Int, Int>>) {
+    override fun toString(): String = this.toSheet().toString()
+
+    fun foldX(col: Int) = SetSheet(dots
+        .partition { (x, _) -> x < col }
+        .let { (same, other) -> same.toSet() + other.map { (x, y) -> (2 * col - x) to y } }
+    )
+
+    fun foldY(row: Int) = SetSheet(dots
+        .partition { (_, y) -> y < row }
+        .let { (same, other) -> same.toSet() + other.map { (x, y) -> x to (2 * row - y) } }
+    )
+
+    private fun toSheet(): Sheet {
+        val (maxX, maxY) = this.dots.reduce { (aX, aY), (pX, pY) -> max(aX, pX) to max(aY, pY) }
+
+        val dots = Array (maxY + 1) { BooleanArray(maxX + 1) }
+        this.dots.forEach { (x, y) ->
+            dots[y][x] = true
+        }
+
+        return Sheet(dots.map(BooleanArray::toList))
+    }
+
+    companion object {
+        enum class FoldAxis { X, Y }
+
+        private fun String.toFoldAxis() = when (this) {
+            "x" -> FoldAxis.X
+            "y" -> FoldAxis.Y
+            else -> error("$this is not a valid FoldAxis")
+        }
+
+        private fun makeFold(axis: FoldAxis, pos: Int): SetFold = { sheet ->
+            when (axis) {
+            FoldAxis.X -> sheet.foldX(pos)
+            FoldAxis.Y -> sheet.foldY(pos)
+            }
+        }
+
+        fun valueOf(text: String): SetSheet =
+            SetSheet(text
+                .split("\r?\n".toRegex())
+                .map { it.split(",").map(String::toInt).let { (x, y) -> x to y } }.toSet())
+
+        fun valueOfAndFolds(text: String): Pair<SetSheet, List<SetFold>> = text
+            .split("(\r?\n){2}".toRegex())
+            .let { (dots, folds) -> valueOf(dots) to
+                    folds
+                        .split("\r?\n".toRegex())
+                        .map { """fold along ([xy])=(\d+)""".toRegex()
+                            .matchEntire(it)!!
+                            .destructured
+                            .let { (a, p) -> makeFold(a.toFoldAxis(), p.toInt()) } } }
+    }
+
+    fun apply(folds: List<SetFold>) = folds.fold(this) { s, f -> f(s) }
+}
+
+typealias SetFold = (SetSheet) -> SetSheet
+
+fun partOneSet(input: String) {
+    println("Part 1 with set: ${SetSheet.valueOfAndFolds(input).let { (s, f) -> s.apply(f.subList(0,1)) }.dots.size}")
+}
+
+fun partTwoSet(input: String) {
+    println("Part 2 with set:\n${SetSheet.valueOfAndFolds(input).let { (s, f) -> s.apply(f) }}")
 }
