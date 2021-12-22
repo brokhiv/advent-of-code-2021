9a8ed9a6d4b8ac8012a6ee44fbb8ec5542d19b59
Ivo_Broekhof <i.broekhof@student.utwente.nl>
2021-12-23 00:16:31 +0100

    Finally hacked day 18 together

--- src/main/kotlin/days/day22/solution.kt
+++ src/main/kotlin/days/day22/solution.kt
@@ -1,24 +1,98 @@
 package days.day22
 
+import util.Quad
 import java.io.File
+import kotlin.math.max
+import kotlin.math.min
 
 fun main() {
+
     val today = "src/main/kotlin/days/day22"
 
     File(today)
         .listFiles { _, name -> name.endsWith(".txt") }
         .forEach {
             println("Input: ${it.name}")
-            val text = it.readText() // TODO processing
+            val text = it.readLines().map(Instruction.Companion::valueOf)
             partOne(text)
             partTwo(text)
         }
 }
 
-fun partOne(input: Any) {
-    println("Part 1: Not implemented yet")
+data class Instruction(val type: Int, val rx: IntRange, val ry: IntRange, val rz: IntRange, val corrector: Long = 0L) {
+    val volume = if (rx.isEmpty() || ry.isEmpty() || rz.isEmpty()) 0L else type *
+            (rx.last - rx.first + 1L) *
+            (ry.last - ry.first + 1L) *
+            (rz.last - rz.first + 1L) +
+            corrector
+
+    fun withCorrector(_corrector: Long) = Instruction(type, rx, ry, rz, _corrector)
+
+    companion object {
+        fun valueOf(text: String): Instruction =
+            """(on|off) x=(-?\d+)\.\.(-?\d+),y=(-?\d+)\.\.(-?\d+),z=(-?\d+)\.\.(-?\d+)"""
+                .toRegex()
+                .matchEntire(text)
+                ?.destructured
+                ?.let { (t, xMin, xMax, yMin, yMax, zMin, zMax) -> Instruction(
+                    if (t == "on") 1 else 0,
+                    xMin.toInt()..xMax.toInt(),
+                    yMin.toInt()..yMax.toInt(),
+                    zMin.toInt()..zMax.toInt()
+                    )
+                }
+                ?: error("Parsing failed")
+    }
 }
 
-fun partTwo(input: Any) {
-    println("Part 2: Not implemented yet")
+fun partOne(input: List<Instruction>) {
+    val cubes = List(101) { List(101) { BooleanArray(101) } } // indices offset by 50
+
+    for ((t, xRange, yRange, zRange) in input.map { (t, xRange, yRange, zRange) ->
+        Instruction(t,
+            max(xRange.first, -50)..min(xRange.last, 50),
+            max(yRange.first, -50)..min(yRange.last, 50),
+            max(zRange.first, -50)..min(zRange.last, 50))
+        }
+    ) {
+        for (x in xRange) {
+            for (y in yRange) {
+                for (z in zRange) {
+                    cubes[x + 50][y + 50][z + 50] = t == 1
+                }
+            }
+        }
+    }
+
+    println("Part 1: ${cubes.sumOf { it.sumOf { it.count { it } } }}")
+}
+
+fun overlaps(instrs: List<Instruction>, inverted: Boolean = false): List<Instruction> = instrs.mapIndexed { i, instr ->
+        var overlap = buildList {
+            for (j in 0 until i) {
+                add(
+                    Instruction(
+                        -instrs[j].type,
+                        max(instr.rx.first, instrs[j].rx.first)..min(instr.rx.last, instrs[j].rx.last),
+                        max(instr.ry.first, instrs[j].ry.first)..min(instr.ry.last, instrs[j].ry.last),
+                        max(instr.rz.first, instrs[j].rz.first)..min(instr.rz.last, instrs[j].rz.last)
+                    )
+                )
+            }
+        }
+        overlap = overlap.filterNot { it.rx.isEmpty() || it.ry.isEmpty() || it.rz.isEmpty() }
+        val corrector = if (overlap.isEmpty()) (if (inverted) 1 else -1) * overlap.reduced() else overlaps(overlap, !inverted).reduced()
+        instr.withCorrector(corrector)
+    }
+
+private fun List<Instruction>.reduced() = this
+    .sumOf { it.volume }
+
+
+fun partTwo(input: List<Instruction>) {
+    val overlaps = overlaps(input)
+
+    val count = overlaps.reduced()
+
+    println("Part 2: $count")
 }
--- src/main/kotlin/util/UsefulStuff.kt
+++ src/main/kotlin/util/UsefulStuff.kt
@@ -1,5 +1,7 @@
 package util
 
+import java.io.Serializable
+
 fun <T> Iterable<T>.countDistinct(): Map<T, Int> = this.distinct().associateWith { this.countEq(it) }
 
 inline fun <T> Iterable<T>.countEq(element: T): Int = this.count { it == element }
@@ -11,3 +13,12 @@ inline fun <T, U> Iterable<T>.nMap(fs: List<(T) -> U>): List<U> = this.zip(fs) {
 fun <A,B,C,D> Pair<A,B>.bimap(f: (A) -> C, g: (B) -> D) = Pair(f(first), g(second))
 
 fun <A,B,C,D,E,F> Triple<A,B,C>.trimap(f: (A) -> D, g: (B) -> E, h: (C) -> F) = Triple(f(first), g(second), h(third))
+
+data class Quad<out A, out B, out C, out D>(
+    val first: A,
+    val second: B,
+    val third: C,
+    val fourth: D
+): Serializable {
+    override fun toString() = "($first, $second, $third, $fourth)"
+}
